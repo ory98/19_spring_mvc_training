c75f41b54ba988acd52b925b23d96df5faa87c92
User <User@F12>
2022-07-18 17:47:51 +0900

    7/18 수업

---
+++ src/main/java/com/spring/training/board/dao/BoardDao.java
@@ -0,0 +1,5 @@
+package com.spring.training.board.dao;
+
+public interface BoardDao {
+
+}
---
+++ src/main/java/com/spring/training/board/service/BoardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.spring.training.board.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+}
